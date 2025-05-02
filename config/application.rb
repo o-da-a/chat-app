aa19db7aa54a10e1f5a7f525fce38fc709e204e2
o-da-a <miu.oda.1381@gmail.com>
2025-05-02 16:42:42 +0900

    フロント実装の準備

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,7 @@ module ChatApp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    config.generatiors do |g|
+    config.generators do |g|
       g.stylesheets false
       g.javascripts false
       g.helper false
